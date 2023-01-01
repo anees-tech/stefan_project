d4044b1d811992a0cf313cebc12b66168110f275
HawiTad <adugtad@gmail.com>
2023-01-01 11:30:26 +0300

    res

--- src/components/pageHero.jsx
+++ src/components/pageHero.jsx
@@ -24,44 +24,49 @@ function PageHero({ children, background }) {
                                 <span className='text-[#FFFFFF80] text-[16px] border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] cursor-pointer'>CONTACT US</span>
                                 <span className='text-[#FFFFFF80] text-[16px] border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] cursor-pointer'>FAQ</span>
                             </div>
-                            {!menuOpen &&
-                                <div className='bg-[#9686DA] tb:hidden flex flex-row items-center justify-between fixed top-0 left-0 z-20 py-3 px-4 pt-5 w-full' >
-                                    <div className='flex flex-row items-center space-x-3'>
-                                        <img src='/assets/logoSvg.svg' className='flex ' />
-                                        <span className='hidden md:flex text-white font-medium text-[19px]'>TREATMENTS</span>
-                                    </div>
-                                    <div className='flex flex-row space-x-3 '>
+                            <div className='bg-[#9686DA] tb:hidden flex flex-row items-center justify-between fixed top-0 left-0 z-20 pt-5 w-full' >
+                                <div className='absolute flex flex-row justify-between top-0 px-4 py-3 bg-[#6C5EA4] w-full'>
+                                    <img src='/assets/logoSvg.svg' className=' tb:hidden' />
+                                    <div className='tb:hidden flex flex-row items-center space-x-3 ' >
                                         <img src='/assets/ShoppingSvg.svg' />
                                         {!menuOpen ?
                                             <FaBars size={25} className="cursor-pointer" color='#fff' onClick={() => setMenuOpen(!menuOpen)} />
-                                            : <IoMdClose size={30} className="cursor-pointer" color='#fff' onClick={() => setMenuOpen(!menuOpen)} />
+                                            : <IoMdClose size={25} className="cursor-pointer" color='#fff' onClick={() => setMenuOpen(!menuOpen)} />
                                         }
                                     </div>
                                 </div>
-                            }
-
-                        </div>
-                        {menuOpen && <div className='md:hidden bg-[#9686DA] text-[#000] fixed z-20 top-0 pt-[70px]  flex flex-col items-center justify-start menu h-full w-full'>
-                            <img src='/assets/logoSvg.svg' className=' md:hidden  absolute left-0 top-5 px-4' />
-                            <div className='tb:hidden flex flex-row items-center space-x-3 absolute top-[17px] right-[-6px] px-4' >
-                                <img src='/assets/ShoppingSvg.svg' />
-                                {!menuOpen ?
-                                    <FaBars size={25} className="cursor-pointer" color='#fff' onClick={() => setMenuOpen(!menuOpen)} />
-                                    : <IoMdClose size={30} className="cursor-pointer" color='#fff' onClick={() => setMenuOpen(!menuOpen)} />
-                                }
                             </div>
-                            <span onClick={() => setMenuOpen(false)} className="text-white text-[16px] font-medium mt-3 cursor-pointer p-1 w-full flex flex-col items-center">HOME</span>
-                            <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[16px] mt-3 cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">TREATMENTS</span>
-                            <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[16px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">PRODUCTS</span>
-                            <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[16px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">CONTACT US</span>
-                            <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[16px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">FAQ</span>
-                            <div className='flex flex-col items-center absolute bottom-3'>
-                                <span className='text-white text-[16px] mb-3'>© AZ TREATMENTS 2022</span>
+                        </div>
+                        {menuOpen && <div className='md:hidden bg-[#9686DA]  text-[#000] fixed z-20 top-[49px]   flex flex-col items-center justify-start menu h-full w-full'>
+                            <div className='flex flex-row items-center bg-[#7F73B1] w-full'>
+                                <img src='/assets/googleSvg.svg' className='h-[50px]' />
+                                <img src='/assets/starsSvg.svg' className='h-[23px] mr-2' />
+                                <span className='text-white text-[16px]'>4.5 rating of 25 reviews </span>
+                            </div>
+                            <div className='flex flex-col space-y-2 items-center w-full'>
+                                <span onClick={() => setMenuOpen(false)} className="text-[#FFF] font-medium text-[18px] mt-3 cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:mb-[-3px] p-1 w-fit flex flex-col items-center">HOME</span>
+                                <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[18px] mt-3 cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">TREATMENTS</span>
+                                <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[18px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">PRODUCTS</span>
+                                <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[18px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">CONTACT US</span>
+                                <span onClick={() => setMenuOpen(false)} className="text-[#FFFFFF80] text-[18px] mt-3  cursor-pointer border-hidden border-b-[3px] border-b-[#fff] hover:border-solid hover:mb-[-3px] p-1 w-fit flex flex-col items-center">FAQ</span>
+                            </div>
+                            <div className='flex flex-col items-center absolute bottom-3 w-full'>
                                 <div className='flex flex-row space-x-4'>
                                     <img src='/assets/instagramSvg.svg' />
                                     <img src='/assets/facebookSvg.svg' />
                                     <img src='/assets/youtubeSvg.svg' />
                                 </div>
+                                <div className='flex flex-row justify-between items-center bg-[#6C5EA4] text-white text-[14px]  px-4 py-1 my-3 w-full'>
+                                    <div className='flex flex-row space-x-3 items-center'>
+                                        <img src='/assets/mailSvg.svg' className='hover:cursor-pointer' />
+                                        <span className='hover:cursor-pointer' >support@aztreatments.com</span>
+                                    </div>
+                                    <div className='flex flex-row space-x-3 items-center'>
+                                        <span className='hover:cursor-pointer'>+44 020 7042 1880</span>
+                                        <img src='/assets/telephoneSvg.svg' className='hover:cursor-pointer' />
+                                    </div>
+                                </div>
+                                <span className='text-white text-[16px] mb-[49px]'>© AZ TREATMENTS 2022</span>
                             </div>
                         </div>}
                         {children}
